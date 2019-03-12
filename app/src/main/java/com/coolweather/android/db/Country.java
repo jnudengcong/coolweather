9c2dab5bc539a2041fe80900b6e90e81caf9a359
dengcong <jnudengcong@gmail.com>
2019-03-12 14:57:20 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/Country.java
@@ -0,0 +1,44 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+//是县不是国家
+public class Country extends LitePalSupport {
+
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int cityId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+}
